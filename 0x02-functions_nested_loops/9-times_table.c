87537575f33852c649e2ee0947d7f3e962385dfe
Kosgey Laban <devynlab@gmail.com>
2022-09-12 16:11:53 +0300

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar('0');
+		putchar(',');
+		putchar(' ');
+		for (y = 0; y <= 9; y++)
+		{
+			z = (x * y);
+			if ((z / 10) > 0)
+			{
+				putchar((z / 10) + '0');
+			}
+			else
+			{
+				putchar(' ');
+			}
+			putchar((z % 10) + '0');
+			if (y < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+
+	return (0);
+}
