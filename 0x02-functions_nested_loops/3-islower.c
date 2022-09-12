aaef28adb3ec794a5dae8b57c81ddcad632ae927
Kosgey Laban <devynlab@gmail.com>
2022-09-12 14:13:41 +0300

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int r = 'P';
+	if (islower(r))
+	{
+		return (1);
+	}
+
+	return (0);
+}
