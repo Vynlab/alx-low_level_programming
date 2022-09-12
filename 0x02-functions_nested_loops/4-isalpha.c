df8bbb5fed7a40273bedfb2e7461ae3a551f9ad8
Kosgey Laban <devynlab@gmail.com>
2022-09-12 14:17:11 +0300

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+	if (isalpha(x));
+	{
+		return (1);
+	}
+
+	return (0);
+}
